2a7e860a6d60007845f1836b2e2859b17777a9aa
TahaBagwala <tahabag8@gmail.com>
2022-10-20 23:55:06 +0530

    chore: all the wiring done (completed till ex 09)

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var output = document.querySelector("#translate-output");
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function constructURL(text) {
+    return serverURL + "?text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Error occured. ", error);
+    alert("Sorry! Something went wrong with the server. Please try again after some time.")
+}
 
 function clickHandler () {
-    output.innerText = "ABCD" + txtInput.value;
-}
+    var inputText = txtInput.value;
+
+    fetch(constructURL(inputText))
+        .then(response => response.json())
+        .then(json => console.log(json.contents.translated))
+        .catch(errorHandler)
+};
 
 btnTranslate.addEventListener("click", clickHandler)
