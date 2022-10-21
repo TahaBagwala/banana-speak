68560b569801f2fa5cbf1567ce15b9b7bb596200
TahaBagwala <tahabag8@gmail.com>
2022-10-21 14:55:34 +0530

    chore: completing the page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var output = document.querySelector("#translate-output");
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function constructURL(text) {
     return serverURL + "?text=" + text;
@@ -17,7 +17,10 @@ function clickHandler () {
 
     fetch(constructURL(inputText))
         .then(response => response.json())
-        .then(json => console.log(json.contents.translated))
+        .then(json => {
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText;
+        })
         .catch(errorHandler)
 };
 
